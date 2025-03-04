219fe53f0ac3d63d8d33b5408125796388e4427d
Vipul Dhiman <46882144+vipuldhiman@users.noreply.github.com>
2025-03-04 19:35:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageCompressor from "./components/imageCompressor";
+// import "./styles.css"; // Import global styles
+
+const App = () => {
+  return (
+    <div>
+      <ImageCompressor />
+    </div>
+  );
+};
+
+export default App;
